3952c07aef890e9e5966a6e0e249d043603815d2
Johan Carrillo <jcarrillob3@miumg.edu.gt>
2021-06-15 09:21:53 -0600

    Se ha modificado la clase PartyApp

--- src/main/java/com/partydecoration/PartyApp.java
+++ src/main/java/com/partydecoration/PartyApp.java
@@ -17,7 +17,9 @@ public class PartyApp {
         partyFacade.orderStereo();
         partyFacade.orderTable();
         System.out.println("------------------------------------------------------------------------------------------");
-        System.out.println("TOTAL\t\t\t\t\t\t\t\tQ1685.00");
+        System.out.println("TOTAL\t\t\t\t\t\t\t\tQ 1482.80");
+        System.out.println("IVA\t\t\t\t\t\t\t\tQ  202.20");
+        System.out.println("TOTAL A PAGAR\t\t\t\t\t\t\tQ 1685.00");
         System.out.println("##########################################################################################");
         
     }
